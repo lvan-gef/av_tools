4fb4ef7267fc94ec5fc5c64ac60d944c1ad61309
Luuk van Geffen <luxesound@MacBook-Pro-van-Luuk.local>
2025-05-19 16:18:13 +0200

    Clean up after converting

--- pdf_too_pptx/main.py
+++ pdf_too_pptx/main.py
@@ -17,6 +17,8 @@ def main(pdf: Path, reso: namedtuple, out: Path) -> None:
     pngs = _pdf_too_png(pdf=pdf, reso=reso, outdir=outdir)
     _png_too_pptx(pngs_list=pngs, pptx=out)
 
+    outdir.rmdir()
+
 
 def _pdf_too_png(pdf: Path, reso: namedtuple, outdir: Path) -> list[Path]:
     outlist_pngs = []
